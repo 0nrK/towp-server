e10ba9f0fe07befc45f1a506f70fa499c523ed05
0nrk <onurkirnapci@gmail.com>
2023-05-23 21:27:28 +0300

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,17 @@ import { promisify } from "./utils/promisify";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://towp.online"
+}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 dotenv.config()
